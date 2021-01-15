8630042efe14677dc215f30a59b172f55b556de3
Eli Hubbert <eli@thehubberts.com>
2021-01-14 21:10:57 -0500

    Update folder_file_remover.py

--- folder_file_remover.py
+++ folder_file_remover.py
@@ -1,15 +1,26 @@
 import os
 import shutil
 
-folderName = 'TestFolder'
+folderName = 'TestFolder'  # Establishes a folder name for directory
 path = os.path.join(os.path.join(os.environ['USERPROFILE']),
                     'Python', 'python-intro-file-management', 'directory', folderName)
+# Taps into the users default profile directory with os.environ
+print('Target path found. . .', path)
+
 fileName = 'test'
 fileType = '.txt'
 file = fileName + fileType
+# Specifies the file name for locating the file path
+# File and folder name are known as they are created earlier in repository
+print('Found file,', file)
 
-filePath = os.path.join(path, file)
-print(filePath)
-os.remove(filePath)
+filePath = os.path.join(path, file)  # Finds the file by directory
+os.remove(filePath)  # Deletes file from within folder
+print('Removing file,', file, "from directory", filePath)
 
-shutil.rmtree(path)
+shutil.rmtree(path)  # Deletes folder
+print('Removing folder,', folderName, "from", path)
+print('\n~~~ All files and folders removed sucessfully. ~~~')
+
+# The deletion of the folder must be executed last otherwise,
+# 'error: folder is not empty' error occurs.
